634b44999bfa82798792231a0bd275be9ca70f13
Gontier <alexis.gontier03@gmail.com>
2024-12-19 14:13:23 +0100

    🔥 [BDD] data

--- backend/db/init.sql
+++ backend/db/init.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS tasks (
   id SERIAL PRIMARY KEY,
   title VARCHAR(255) NOT NULL,
   description TEXT NOT NULL,
-  -- created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 INSERT INTO tasks (title, description) VALUES ('Faire le projet DevOps', 'Finir le projet DevOps pour la soutenance');
