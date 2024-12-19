39ef34b2a1290abf6d0a99da17d44af458cc0f96
Gontier <alexis.gontier03@gmail.com>
2024-12-19 11:50:51 +0100

    🚀 [BACKEND] import de cors

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import dotenv from 'dotenv';
 import taskListRoute from './routes/taskListRoute.js';
 
@@ -7,6 +8,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
